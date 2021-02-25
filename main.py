4072744ca9dfb31d00e86c6a28a4506e4950e6c3
Viliam Mihálik <viliam.mihalik@smartbase.sk>
2021-02-25 17:35:19 +0100

    triv: added basic read

--- main.py
+++ main.py
@@ -3,10 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def read_input(file_name):
+    with open(file_name) as f:
+        lines = f.readlines()
+        for line in lines:
+            print(line)
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    read_input('./input/main.txt')
 
 
 # Press the green button in the gutter to run the script.
