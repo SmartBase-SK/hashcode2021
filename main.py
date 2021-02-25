468a4b11168cccc016ca286c0abe4213544d1c3f
Viliam Mihálik <viliam.mihalik@smartbase.sk>
2021-02-25 19:44:04 +0100

    triv: read

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Solution:
         self.NUM_STREETS = None
         self.NUM_CARS = None
         self.BONUS = None
-
+        self.BEST_CARS_STREET = {}
 
     def read_input(self, file_name):
         with open(file_name) as f:
@@ -24,7 +24,7 @@ class Solution:
                 line = self.data[line_num]
                 START, END, NAME, LEN = line.split(' ')
                 self.STREETS[NAME] = {'start': int(START), 'end': int(END), 'len': int(LEN)}
-                self.STREETS_LENS[NAME] = LEN
+                self.STREETS_LENS[NAME] = int(LEN)
 
             for i in range(self.NUM_CARS):
                 line_num += 1
@@ -32,11 +32,35 @@ class Solution:
                 CAR_STREETS = line.split(' ')[1:]
                 self.CARS[i] = list(CAR_STREETS)
 
+    def path_length(self, path):
+        path_score = 0
+        for street in path[1:]:
+            path_score += 1 + self.STREETS_LENS[street]
+        return path_score
 
     def run(self):
         self.read_input(self.input_file_name)
+        for key, streets in self.CARS.items():
+            self.BEST_CARS_STREET[key] = self.path_length(streets)
+
+        result = sorted(self.BEST_CARS_STREET, key=self.BEST_CARS_STREET.get)
+
+        result_data = {}
+        for CAR_ID in result[:1]:
+            STREETS_TO_GO = self.CARS[CAR_ID]
+            for STREET in STREETS_TO_GO:
+                street = self.STREETS[STREET]
+                result_data[street['end']] = {STREET: 1}
+
+        result_lines = []
+        result_lines.append(str(len(result_data)))
+        for intersection_id, streets in result_data.items():
+            result_lines.append(str(intersection_id))
+            result_lines.append(str(len(streets)))
+            for street_name, seconds in streets.items():
+                result_lines.append(f'{street_name} {seconds}')
         with open(self.output_file_name, 'w') as f:
-            f.writelines(self.data)
+            f.writelines('\n'.join(result_lines))
 
 
 s = Solution()
