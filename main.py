d3be5989644494bbd0eefe7d226df872a1507ca4
igerko <jakub.kacur@gmail.com>
2021-02-25 20:33:27 +0100

    pop unused streets

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Solution:
         self.NUM_CARS = None
         self.BONUS = None
         self.BEST_CARS_STREET = {}
+        self.used_streets_by_cars = set()
 
     def read_input(self, file_name):
         with open(file_name) as f:
@@ -37,16 +38,26 @@ class Solution:
         for street in path[1:]:
             path_score += 1 + self.STREETS_LENS[street]
         return path_score
+    
+    def pop_unused_streets(self):
+        all_streets = set(street_name for street_name in self.STREETS)
+        streets_to_remove = all_streets - self.used_streets_by_cars
+        print(f'removing streets {len(streets_to_remove)}')
+        for street_name in streets_to_remove:
+            self.STREETS_LENS.pop(street_name)
+            self.STREETS.pop(street_name)
 
     def run(self):
         self.read_input(self.input_file_name)
         for key, streets in self.CARS.items():
+            self.used_streets_by_cars.update(streets)
             path_length = self.path_length(streets)
             if path_length <= self.DURATION:
                 self.BEST_CARS_STREET[key] = path_length
 
         result = sorted(self.BEST_CARS_STREET, key=self.BEST_CARS_STREET.get)
 
+        self.pop_unused_streets()
 
         # state = {}
         # for time in enumerate(self.DURATION):
